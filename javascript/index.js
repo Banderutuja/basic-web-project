6ff1acf6302b2eac0c535a3e37203d6436684726
shivrajbande <shivabande09@gmail.com>
2024-08-20 08:04:38 +0530

    added javascript file

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+function changeParaTagValue() {
+  //with id
+  //method-1
+  // //get para tag with id
+  // let variableRutu = document.getElementById("para").innerHTML;
+
+  // //update the value of paratag
+  // variableRutu = "rutu page"
+
+  // //re-assign the para tag with updated value
+  // document.getElementById("para").innerHTML = variableRutu;
+
+  //method-2
+  document.getElementById("para").innerHTML = "rutu page";
+// console.log("wrerhweth");
+  //with class
+
+//   let x = document.getElementsByClassName("para").item;
+}
